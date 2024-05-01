6e3d347140f1c62c7dda9be35427767e2742cb60
Javi Merino <merino.jav@gmail.com>
2024-05-01 20:28:16 +0100

    mail_replyer: format with black

--- mail_replyer/__init__.py
+++ mail_replyer/__init__.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 from pathlib import Path
 
+
 def generate_prompt(context_file: Path, instructions_file: Path):
     env = jinja2.Environment(loader=jinja2.PackageLoader("mail_replyer"))
     prompt_template = env.get_template("prompt_template.txt")
@@ -14,13 +15,15 @@ def generate_prompt(context_file: Path, instructions_file: Path):
 
     return prompt_template.render(context=ctxt, additional_instructions=instructions)
 
+
 def generate_email(context: str | os.PathLike, instructions: str | os.PathLike):
     context_file = Path(context)
     instructions_file = Path(instructions)
     prompt = generate_prompt(context_file, instructions_file)
-    #ollama_reply = ollama.generate(model=MODEL, prompt=prompt)
+    # ollama_reply = ollama.generate(model=MODEL, prompt=prompt)
     # print(ollama_reply['response'])
     print(prompt)
+
 
 def main():
     fire.Fire(generate_email)
