3ec7d44920f5da2608d7c71f38c76590a2966e75
Javi Merino <merino.jav@gmail.com>
2024-05-01 20:28:10 +0100

    prompt: add additional instructions

--- mail_replyer/__init__.py
+++ mail_replyer/__init__.py
@@ -3,18 +3,21 @@ import jinja2
 import os
 from pathlib import Path
 
-def generate_prompt(context_file: Path):
+def generate_prompt(context_file: Path, instructions_file: Path):
     env = jinja2.Environment(loader=jinja2.PackageLoader("mail_replyer"))
     prompt_template = env.get_template("prompt_template.txt")
 
+    with open(instructions_file) as inst_file:
+        instructions = inst_file.read()
     with open(context_file) as ctxt_file:
         ctxt = ctxt_file.read()
 
-    return prompt_template.render(context=ctxt)
+    return prompt_template.render(context=ctxt, additional_instructions=instructions)
 
-def generate_email(context: str | os.PathLike):
+def generate_email(context: str | os.PathLike, instructions: str | os.PathLike):
     context_file = Path(context)
-    prompt = generate_prompt(context_file)
+    instructions_file = Path(instructions)
+    prompt = generate_prompt(context_file, instructions_file)
     #ollama_reply = ollama.generate(model=MODEL, prompt=prompt)
     # print(ollama_reply['response'])
     print(prompt)
