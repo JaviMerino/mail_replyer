ffd7631c9dc1e795ab62247da028b78994d6859f
Javi Merino <merino.jav@gmail.com>
2024-05-01 20:28:35 +0100

    mail_replyer: add documentation

--- mail_replyer/__init__.py
+++ mail_replyer/__init__.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 
 def generate_prompt(context_file: Path, instructions_file: Path):
+    """Read the context and the instructions, and apply it to the template to generate a prompt for the language model"""
     env = jinja2.Environment(loader=jinja2.PackageLoader("mail_replyer"))
     prompt_template = env.get_template("prompt_template.txt")
 
@@ -19,6 +20,7 @@ def generate_prompt(context_file: Path, instructions_file: Path):
 
 
 async def print_reply(model: str, prompt: str):
+    """Stream the ollama reply"""
     client = ollama.AsyncClient()
     async for part in await client.generate(model=model, prompt=prompt, stream=True):
         print(part["response"], end="", flush=True)
@@ -28,6 +30,13 @@ async def print_reply(model: str, prompt: str):
 def generate_email(
     context: str | os.PathLike, instructions: str | os.PathLike, model: str = "mistral"
 ):
+    """Generate an email reply.
+
+    Args:
+      context: A file providing extra context to the language model. For example, "John is a manager at Banana Tech".
+      instructions: A file with the instructions.  For example, "Confirm that you are available on Monday".
+      model: The language model to use
+    """
     context_file = Path(context)
     instructions_file = Path(instructions)
     prompt = generate_prompt(context_file, instructions_file)
